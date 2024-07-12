e3c0df4c4449f08c1817697eba9982b37604c7ba
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-07-12 11:45:35 +0300

    add controllers

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -15,3 +15,8 @@ export const updateContactSchema = Joi.object({
     'object.min': 'Body must have at least one field',
     'string.email': 'Invalid email format'
   });
+
+  
+export const editFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
