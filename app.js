4adc1d48dafe9b652f3d3f524e2b13f575f0037d
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-07-12 11:25:46 +0300

    add structure

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
-
+import authRouter from "./routes/authRouter.js";
 const app = express();
 
 app.use(morgan("tiny"));
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/auth", authRouter);
+
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
@@ -25,5 +27,6 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
     console.log("Server is running. Use our API on port: 3000");
   });
+
 
 
